510e1a1e9e933f6d3101c29e1a59508a67dacf2c
Castor Dunkels <castordnk@gmail.com>
2021-11-09 14:50:25 +0100

    Start

---
+++ Slagsmål_spel/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+        int AdolfHitler = 1000000000;
+
+        int JeffBezos = 1000000000;
+
+        string Jeffname = "Jeff";
+
+        string Adolfname = "Adolf";
+
+        Random.generator = new Random();
+        while (AdolfHitler > 0 && JeffBezos > 0)
+        {
+            System.Console.WriteLine("Racial cleaner vs guy who went into space with a genetalia shaped rocket");
+             Console.ReadLine();
+             
+        }
+ 
