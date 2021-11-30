ecf6d71217fdad0dba401335e1a2e247de715769
Castor Dunkels <castordnk@gmail.com>
2021-11-30 11:01:06 +0100

    start

--- Slagsmål_spel/Program.cs
+++ Slagsmål_spel/Program.cs
@@ -3,15 +3,30 @@
 
         int JeffBezos = 1000000000;
 
-        string Jeffname = "Jeff";
+        string jeffname = "Jeff";
 
-        string Adolfname = "Adolf";
+        string adolfname = "Adolf";
 
-        Random.generator = new Random();
+        int round = 0;
+
+        Console.WriteLine("Racial cleaner vs guy who went into space with a genetalia shaped rocket");
+        Random generator = new Random();
         while (AdolfHitler > 0 && JeffBezos > 0)
         {
-            System.Console.WriteLine("Racial cleaner vs guy who went into space with a genetalia shaped rocket");
-             Console.ReadLine();
-             
+            Console.WriteLine($"Begin round {round}");
+            Console.WriteLine($"{jeffname} {JeffBezos} {adolfname} {AdolfHitler}");
+            Console.ReadKey();
+            round ++; 
+
+
+            int jeffdmg = generator.Next(1000000000);
+            AdolfHitler -= jeffdmg;
+            AdolfHitler = Math.Max(0, AdolfHitler);
+            Console.WriteLine($"{jeffname} deals {jeffdmg} dmg to {adolfname}");
+
+            int adolfdmg = generator.Next(1000000000);
+            JeffBezos -= adolfdmg;
+            JeffBezos = Math.Max(0, JeffBezos);
+            Console.WriteLine($"{adolfname} deals {adolfdmg} dmg to {jeffname}");
         }
  
