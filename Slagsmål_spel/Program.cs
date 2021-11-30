5629c3e92506cb71b0c7347a14b48ecf3e839102
Castor Dunkels <castordnk@gmail.com>
2021-11-30 15:08:22 +0100

    continue

--- Slagsmål_spel/Program.cs
+++ Slagsmål_spel/Program.cs
@@ -1,32 +1,42 @@
 ﻿using System;
-        int AdolfHitler = 1000000000;
+int AdolfHitler = 1000000000;
 
-        int JeffBezos = 1000000000;
+int JeffBezos = 1000000000;
 
-        string jeffname = "Jeff";
+string jeffname = "Jeff";
 
-        string adolfname = "Adolf";
+string adolfname = "Adolf";
 
-        int round = 0;
+int round = 0;
 
-        Console.WriteLine("Racial cleaner vs guy who went into space with a genetalia shaped rocket");
-        Random generator = new Random();
-        while (AdolfHitler > 0 && JeffBezos > 0)
-        {
-            Console.WriteLine($"Begin round {round}");
-            Console.WriteLine($"{jeffname} {JeffBezos} {adolfname} {AdolfHitler}");
-            Console.ReadKey();
-            round ++; 
+Console.WriteLine("Racial cleaner vs guy who went into space with a genetalia shaped rocket\n press any button to start");
+Console.ReadKey();
 
 
-            int jeffdmg = generator.Next(1000000000);
-            AdolfHitler -= jeffdmg;
-            AdolfHitler = Math.Max(0, AdolfHitler);
-            Console.WriteLine($"{jeffname} deals {jeffdmg} dmg to {adolfname}");
+Random generator = new Random();
+while (AdolfHitler > 0 && JeffBezos > 0)
+{
+    Console.Clear();
+    Console.WriteLine($"Begin round {round}");
+    Console.WriteLine($"{jeffname} {JeffBezos} Hp {adolfname} {AdolfHitler} Hp");
+    Console.ReadKey();
+    round++;
 
-            int adolfdmg = generator.Next(1000000000);
-            JeffBezos -= adolfdmg;
-            JeffBezos = Math.Max(0, JeffBezos);
-            Console.WriteLine($"{adolfname} deals {adolfdmg} dmg to {jeffname}");
-        }
- 
+
+    int jeffdmg = generator.Next(1000000000);
+    AdolfHitler -= jeffdmg;
+    AdolfHitler = Math.Max(0, AdolfHitler);
+    Console.WriteLine($"{jeffname} deals {jeffdmg} dmg to {adolfname}");
+
+    int adolfdmg = generator.Next(1000000000);
+    JeffBezos -= adolfdmg;
+    JeffBezos = Math.Max(0, JeffBezos);
+    Console.WriteLine($"{adolfname} deals {adolfdmg} dmg to {jeffname}");
+
+    Console.WriteLine("Press any button to continue");
+    Console.ReadKey();
+}
+
+Console.WriteLine("------------------------------------Fight over------------------------------");
+Console.ReadLine();
+
